6fb9b1f5158701eb7e96ee57393f96fe8ad2e4df
Aman-rana83 <11022210031@stu.srmuniversity.ac.in>
2024-12-01 21:48:06 +0530

    ok
    ok

---
+++ TK159229-Machine Learning for Real-Time Fuel Consumption Prediction and Driving Profile Classification Based on ECU Data/FRONTEND/app.py
@@ -0,0 +1,110 @@
+from flask import Flask,render_template,redirect,request,url_for, send_file
+import mysql.connector
+from flask import Flask, request, render_template
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
+from sklearn.linear_model import Ridge
+from sklearn.ensemble import AdaBoostRegressor
+from xgboost import XGBRFRegressor
+from sklearn.svm import SVR
+import joblib
+
+app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    port="3306",
+    database='fuel'
+)
+
+mycursor = mydb.cursor()
+
+def executionquery(query,values):
+    mycursor.execute(query,values)
+    mydb.commit()
+    return
+
+def retrivequery1(query,values):
+    mycursor.execute(query,values)
+    data = mycursor.fetchall()
+    return data
+
+def retrivequery2(query):
+    mycursor.execute(query)
+    data = mycursor.fetchall()
+    return data
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        c_password = request.form['c_password']
+        if password == c_password:
+            query = "SELECT UPPER(email) FROM users"
+            email_data = retrivequery2(query)
+            email_data_list = []
+            for i in email_data:
+                email_data_list.append(i[0])
+            if email.upper() not in email_data_list:
+                query = "INSERT INTO users (email, password) VALUES (%s, %s)"
+                values = (email, password)
+                executionquery(query, values)
+                return render_template('login.html', message="Successfully Registered!")
+            return render_template('register.html', message="This email ID is already exists!")
+        return render_template('register.html', message="Conform password is not match!")
+    return render_template('register.html')
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        
+        query = "SELECT UPPER(email) FROM users"
+        email_data = retrivequery2(query)
+        email_data_list = []
+        for i in email_data:
+            email_data_list.append(i[0])
+
+        if email.upper() in email_data_list:
+            query = "SELECT UPPER(password) FROM users WHERE email = %s"
+            values = (email,)
+            password__data = retrivequery1(query, values)
+            if password.upper() == password__data[0][0]:
+                global user_email
+                user_email = email
+
+                return render_template('home.html')
+            return render_template('login.html', message= "Invalid Password!!")
+        return render_template('login.html', message= "This email ID does not exist!")
+    return render_template('login.html')
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
